9912d28b906e613dd0a8cd32e50e09e5473c403a
rahul.kataria <rahul.kataria@saarathi.ai>
2023-11-15 22:22:57 +0530

    modify producer.py file

--- python/producer.py
+++ python/producer.py
@@ -5,26 +5,45 @@ from random import choice
 from kafka import KafkaProducer
 
 kafka_server = ["localhost:9092"]
-
 topic = "my-topic-rk"
 
-producer = KafkaProducer(
-    bootstrap_servers=kafka_server,
-    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-)
+def get_producer_connection():
+    try:
+        producer = KafkaProducer(
+            bootstrap_servers=kafka_server,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        )
+        return True, producer
+    except Exception as e:
+        print("error_in producer :", e)
+        return False, None
+    
 
-random_values = [1, 2, 3, 4, 5, 6, 7]
 
-while True:
-    random_value = choice(random_values)
-    data = {
-        "test_data": {
-            "random_value": random_value
-        },
-        "timestamp": str(datetime.now()),
-        "value_status": "High" if random_value > 5 else "Low"
-    }
-    print(data)
-    producer.send(topic, data)
-    producer.flush()
-    sleep(3)
+
+def produce_messages(producer):
+    while True:
+        random_values = [1, 2, 3, 4, 5, 6, 7]
+        random_value = choice(random_values)
+        data = {
+            "test_data": {
+                "random_value": random_value
+            },
+            "timestamp": str(datetime.now()),
+            "value_status": "High" if random_value > 5 else "Low"
+        }
+        print(data)
+        producer.send(topic, data)
+        producer.flush()
+        sleep(3)
+
+
+print("Connecting with Kafka_Producer")
+status, producer = get_producer_connection()
+if status:
+    print("Connection Made with kafka_Producer___")
+    produce_messages(producer)
+else:
+    print("exiting Unable to connection with kafka Producer")
+
+
