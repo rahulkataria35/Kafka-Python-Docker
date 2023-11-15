e1ba711b9983788ab48564595d69fbe289d8039d
rahul.kataria <rahul.kataria@saarathi.ai>
2023-11-16 00:17:09 +0530

    update db

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,9 @@ RUN pip install -r requirements.txt
 COPY app/producer.py .
 COPY app/consumer.py .
 COPY app/main.py .
-
-CMD [ "python", "main.py" ]
+# RUN sudo docker exec -it kafka_server /bin/sh
+# RUN cd ./opt 
+# RUN cd kafka
+# RUN ./bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic my-topic-rk 
+# RUN exit
+# CMD [ "python", "producer.py", "&&", "python", "consumer.py" ]
--- app/database.py
+++ app/database.py
@@ -1,53 +1,58 @@
 import json
 import sqlite3
 
+# Create a new database connection
+
 def create_connection():
     conn = None
     try:
-        conn = sqlite3.connect('mydatabase.db')
+        conn = sqlite3.connect('mydatabase.db')     
         print("Connected to the database")
     except sqlite3.Error as e:
         print(e)
     
     return conn
 
+# Create a table
 def insert_record(conn, record):
-    sql = '''INSERT INTO records(name, age, email) VALUES(?, ?, ?)'''
-    cur = conn.cursor()
-    cur.execute(sql, record)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS employees
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)''')
+    
+    sql = "INSERT INTO employees (name, age) VALUES (?, ?)"
+    cursor.execute(sql, record)
     conn.commit()
     print("Record inserted successfully")
 
+
+
 def extract_data(conn):
     cur = conn.cursor()
-    cur.execute("SELECT * FROM records")
+    cur.execute("SELECT * FROM employees")
     rows = cur.fetchall()
 
     data = []
     for row in rows:
-        record = {'name': row[0], 'age': row[1], 'email': row[2]}
+        record = {'name': row[1], 'age': row[2]}
         data.append(record)
+    json_data = json.dumps(data, indent=4)
 
-    return data
-
-def convert_to_json(data):
-    json_data = json.dumps(data)
     return json_data
 
 # Main program
 conn = create_connection()
 
 # Insert records into the database
-record1 = ('John Doe', 30, 'johndoe@example.com')
-record2 = ('Jane Smith', 25, 'janesmith@example.com')
+record1 = ('John Doe', 30)
+record2 = ('Jane Smith', 25)
 
 insert_record(conn, record1)
 insert_record(conn, record2)
 
 # Extract data from the database and convert to JSON
 data = extract_data(conn)
-json_data = convert_to_json(data)
-print(json_data)
+# json_data = convert_to_json(data)
+print(data)
 
 # Close the database connection
 conn.close()
